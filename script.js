72cd0688e3eaa2ce336e29305e6822e2b39077e8
23129ps <23129ps@hvhs.school.nz>
2025-06-12 01:47:59 +0000

    Jun 12, 2025, 1:47 AM

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-// Part 2 Project
-// Created by Priya Silwal
-// Term 2
+
 
 // Variables
 let insuranceCost = 0;
@@ -10,7 +8,7 @@ let pricePerDay = 0;
 let lateFees = 0;
 let carChoice = "";
 let rentalDays = 0;
-let carIndex;
+  let carFound = false;
 let discountAmount = 0;
 let insuranceOption= "";
 let discountOption ="";
@@ -22,9 +20,9 @@ const cars = ["Solo EV", "Smart Fortwo", "Mazda RX-8", "Toyota Camry", "Honda CR
     "Chevrolet Suburban", "Mercedes-Benz Sprinter", "Ford Transit", "Toyota HiAce"
 ];
 
-const prices = [50, 40, 70, 55, 60, 85, 100, 90, 80]; // Prices in NZD
-const insuranceRates = [10, 8, 12, 9, 10, 15, 20, 18, 17]; // Insurance per day
-const lateFeeRates = [20, 15, 25, 18, 20, 30, 35, 32, 30]; // Late return penalty per day
+const prices = [50, 40, 70, 55, 60, 80, 85, 90, 80]; // Prices in NZD
+const insuranceRates = [10, 11, 12, 13, 14, 15, 20, 21, 27]; // Insurance per day
+const lateFeeRates = [20, 21, 25, 27, 28, 30, 35, 36, 37]; // Late return penalty per day
 const discountOffers = ["SUMMER", "EARLYBIRD", "WEEKLY10"]; // Discount codes
 
 // Function to redirect pages
@@ -70,13 +68,18 @@ function redirectToCarChoice() {
 }
 function redirectToDays() {
     carChoice = document.getElementById("i_carChoice").value;
-    carIndex = cars.indexOf(carChoice);
-    localStorage.setItem("carChoice", carChoice);
     errorElement = document.getElementById("carError");
 
     if (!carChoice) {
         errorElement.textContent = "Please fill out this required field.";
         return;
+    }
+         for (let i = 0; i < cars.length; i++) {
+        if (carChoice === cars[i]) {
+            carFound = true;
+            alert(`User selected: ${cars[i]}`);
+            break; 
+        }
     }
     window.location.href = "days.html";
 }
@@ -96,60 +99,6 @@ function redirectToInsurance() {
         errorElement.textContent = "Please fill out this required field.";
         return;
     }
-    window.location.href = "insurance.html";
+    alert(`User selected: ${prices[i]}`);
 }
 
-function redirectToDiscount() {
-    insuranceOption = document.getElementById("i_insurance").value;
-    errorElement = document.getElementById("insuranceError");
-    errorElement.textContent = "";
-    carIndex = localStorage.getItem("carIndex");
-    rentalDays = localStorage.getItem("rentalDays");
-    if (insuranceOption == "Yes") {
-        insuranceCost = insuranceRates[carIndex] * rentalDays;
-    }
-    else if (!insuranceOption){
-        errorElement.textContent = "Please fill out this required field.";
-        return;
-    }
-    else {
-        insuranceCost = 0;
-    }
-    localStorage.setItem("insuranceCost", insuranceCost);
-    window.location.href = "discounts.html";
-}
-
-function redirectToDiscountCode() {
-    discountOption = document.getElementById("i_discount").value;
-    errorElement = document.getElementById("discountError");
-      errorElement.textContent = "";
-
-    if (discountOption === "Yes"){
-         window.location.href = "discount_code.html";
-    }
-    else if (!discountOption){
-      errorElement.textContent = "Please fill out this required field.";
-    }
-    else{
-
-         window.location.href = "summary.html";
-    }
-}
-
-function redirectToSummary() {
-    errorElement = document.getElementById("discountCodeError"); 
-    discountCode = document.getElementById("i_discountCode").value.trim();
-
-    // Calculate total cost with discount if applicable
-    totalCost = pricePerDay * rentalDays + insuranceCost;
-
-    if (discountOffers.includes(discountCode)) {
-        discountAmount = 0.1 * totalCost;
-        totalCost = totalCost - discountAmount;
-    }
-    else {
-        errorElement.textContent = "Sorry, you have entered a wrong discount code.";
-        return;
-    }
-    window.location.href = "summary.html";
-}
